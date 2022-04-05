1d8cc366cfc73a645d3e20ff25759da83de02997
cleasholdorf <you@example.com>
2022-04-05 19:56:40 -0300

    primeiro

---
+++ AULA 05-04/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AULA_05_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //47 9 9167 2505
+            string numero = Console.ReadLine();
+            string telefone = '(' + numero.Substring(0, 2) +
+            ')' + numero.Substring(2, 1)+ " " + numero.Substring(3, 4) + "-" +
+            numero.Substring(7, 4);
+            Console.WriteLine(telefone);
+        }
+    }
+}
